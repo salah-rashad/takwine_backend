e87cbbefd35e5524bb68bfd8451f03d58afb96a8
Salah Rashad <salah.r.ahmed@gmail.com>
2025-05-12 09:53:35 +0300

    refactor settings for production security; use environment variables for SECRET_KEY and DEBUG

--- takwine/settings.py
+++ takwine/settings.py
@@ -10,10 +10,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*3ib*1!*_hk0wfkn*5csxfi=*c(8b6xe1gfvz00jmmxx7odx@_'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ["192.168.1.10", "127.0.0.1", "localhost", "salahr.pythonanywhere.com", '.onrender.com']
 
